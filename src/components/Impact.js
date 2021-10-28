cc874a982d3687f42ff2f8ce98d69d8ab8f8736e
niranjana687 <niranjana687@gmail.com>
2021-10-28 10:50:26 +0530

    impact section  completed

--- src/components/Impact.js
+++ src/components/Impact.js
@@ -1,17 +1,38 @@
 import React from "react";
+import "../styles/style.css";
 
 export default function Impact() {
     return (
         <div>
-         <h3>Impact</h3>
-            <p>In it's first year, MEC MUNSoC spear headed the Millennium Fellowship by the United Nations at MEC.</p>
-            <p>
+         <h3>IMPACT</h3>
+            <p className="impact-desc">In it's first year, MEC MUNSoC spear headed the Millennium Fellowship by the United Nations at MEC.
+            
             Same year saw MEC being recognised by the IARC center for United Nations for implementing RIO+24 War & Peace - International Decade
             for the Reapproachement of Cultures and IDRC India Program.
+            <br /><br />
+            Later in the year, MEC MUNSOC conducted MEC MUN that had around 100+ particpants from different parts of the state.
+            2020 saw various flagship events by the club including a very successful IntraMUN(the first in the state) and the first e-MUN, which was a fundraiser to 
+            the Chief Ministers disaster relief fund.
+            In it's first year, MEC MUNSoC spear headed the Millennium Fellowship by the United Nations at MEC.
+            <br /><br />
+            Same year saw MEC being recognised by the IARC center for United Nations for implementing RIO+24 War & Peace - International Decade
+            for the Reapproachement of Cultures and IDRC India Program.
+            <br /><br />
+            Later in the year, MEC MUNSOC conducted MEC MUN that had around 100+ particpants from different parts of the state.
+            2020 saw various flagship events by the club including a very successful IntraMUN(the first in the state) and the first e-MUN, which was a fundraiser to 
+            the Chief Ministers disaster relief fund.
+            <br /><br />
+
+            In it's first year, MEC MUNSoC spear headed the Millennium Fellowship by the United Nations at MEC.
+            <br /><br />
+            Same year saw MEC being recognised by the IARC center for United Nations for implementing RIO+24 War & Peace - International Decade
+            for the Reapproachement of Cultures and IDRC India Program.
+            <br /><br />
+            Later in the year, MEC MUNSOC conducted MEC MUN that had around 100+ particpants from different parts of the state.
+            2020 saw various flagship events by the club including a very successful IntraMUN(the first in the state) and the first e-MUN, which was a fundraiser to 
+            the Chief Ministers disaster relief fund.
+            
             </p>
-            <p>Later in the year, MEC MUNSOC conducted MEC MUN that had around 100+ particpants from different parts of the state.</p>
-            <p>2020 saw various flagship events by the club including a very successful IntraMUN(the first in the state) and the first e-MUN, which was a fundraiser to 
-            the Chief Ministers disaster relief fund.</p>
         </div>
     );
 }
